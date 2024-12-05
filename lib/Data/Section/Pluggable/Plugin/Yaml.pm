7f5fa7913988898b863790e7928fe7cc35bbbe64
Graham Ollis <plicease@cpan.org>
2024-12-05 04:43:36 -0700

    Initial structure

---
+++ lib/Data/Section/Pluggable/Plugin/Yaml.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Data::Section::Pluggable::Plugin::Yaml {
+
+  # ABSTRACT: Data::Section::Pluggable Plugin for YAML
+}
+
+1;
+
